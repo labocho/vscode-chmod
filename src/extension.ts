b97e1abc7c1f91ebb4700bc35a0232e1a04140f0
labocho <labocho@penguinlab.jp>
2019-06-18 14:38:02 +0900

    asks new permission

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawnSync } from 'child_process';
+import { TextDecoder } from 'util';
+
+function getCurrentFileName(): string | null {
+	const t = vscode.window.activeTextEditor;
+	if (t === undefined) { return null; }
+	return t.document.fileName;
+}
+
+function sh(cmd: string, args: Array<string>): string | null {
+	const r = spawnSync(cmd, args, { cwd: vscode.workspace.rootPath, encoding: "buffer" });
+	if (r.status !== 0) {
+		const s = new TextDecoder().decode(r.stderr);
+		vscode.window.showErrorMessage(s);
+		return null;
+	}
+
+	return new TextDecoder().decode(r.stdout);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "chmod-x" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.chmod', () => {
+		const filename = getCurrentFileName();
+		if (filename === null) { return; }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const ls_out = sh("ls", ["-l", filename]);
+		if (ls_out === null) { return; }
+		const current_mode = ls_out.replace(/ .*/, "");
+
+		vscode.window.showInputBox({ prompt: `Input chmod's 2nd argument (now: ${current_mode})`, value: "+x"}).then((mode) => {
+			if (typeof(mode) !== "string") { return; }
+
+			if (sh("chmod", [mode, filename]) !== null) {
+				vscode.window.showInformationMessage(`Done: chmod ${mode} ${filename}`);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
